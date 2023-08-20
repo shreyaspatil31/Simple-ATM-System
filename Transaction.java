c6b45e4a1e16aa8990f6ee920fb2ea1b51bf31d7
Shreyas Patil <shreyaspatil554@gmail.com>
2023-08-20 21:46:58 +0530

    Transaction Class

---
+++ Transaction.java
@@ -0,0 +1,25 @@
+import java.time.LocalDateTime;
+
+public class Transaction {
+    private String type;
+    private double amount;
+    private LocalDateTime timestamp;
+
+    public Transaction(String type, double amount) {
+        this.type = type;
+        this.amount = amount;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+}
